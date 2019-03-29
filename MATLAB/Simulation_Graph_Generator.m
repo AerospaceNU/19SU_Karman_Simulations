fd5637518e73b04d3c39fa27ac500445e2768332
AIAA Northeastern <aiaanortheastern@gmail.com>
2019-03-28 20:18:08 -0400

    Minor Changes

--- MATLAB/Simulation_Graph_Generator.m
+++ MATLAB/Simulation_Graph_Generator.m
@@ -2,12 +2,12 @@ close all; clear all;
 %written by Isaac Kramer and Alex Gushulak on 25 February 2019
 
 %% set directory to appropriate location based on local GitHub
-cd C:\Users\isaac\Documents\GitHub\19SP_Karman_Simulations\MATLAB\RASAero_Data\FullScale_MinDiameterBooster;
+%cd C:\Users\AIAA\Documents\GitHub\19SP_Karman_Simulations\MATLAB\RASAero_Data\FullScale_MinDiameterBooster;
 %% Data Assigned to Variables
 data_file = xlsread('20190311_MinDiameterBooster_RASAeroFlightData.xlsx'); 
 
 time_sec = data_file(:,1);
-time_min = time_sec./60; % Convert to minutes
+time_min = time_sec./60
 mach_number = data_file(:,4);
 thrust = data_file(:,8);
 weight = data_file(:,9);
@@ -18,15 +18,15 @@ velocity = data_file(:,18);
 altitude = data_file(:,23);
 
 %% Plotting
-plot1 = plot(time_min, thrust);
-xlim([0 2.8]);
+plot1 = plot(time_sec, stability);
+xlim([0 155]);
 plot1(1).LineWidth = 3;
-plot1(1).Color = [.80,.22,.40];
-set(gca,'FontSize',12)
-xlabel ('Time (mins)', 'FontSize', 30);
-ylabel ('Thrust (lb)', 'FontSize', 30);
+plot1(1).Color = [.95,.15,.15];
+set(gca,'FontSize',12);
+xlabel ('Time (sec)', 'FontSize', 25);
+ylabel ('Stability (calibers)', 'FontSize', 25);
 
-title ('Thrust Profile', 'FontSize', 30);
+title ('Flight Stability', 'FontSize', 25);
 set(gcf, 'color', 'w'); % set graph background to white
 grid on;
 % Note: Air Density at 42,000' = 0.00287802 kg/m^3
